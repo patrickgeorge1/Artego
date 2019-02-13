94651fdd8b59f63fa94367d43d1099d8f5cbee05
patrickgeorge1 <vtgaura@gmail.com>
2019-02-13 03:07:05 +0200

    Made to get all info from text-Boxes
    Now pick the info from textBoxt and write to a file named record.txt

--- main.cpp
+++ main.cpp
@@ -7,11 +7,14 @@
 #include <tchar.h>
 #include <windows.h>
 #include <iostream>
+#include <string>
+#include <fstream>
 
 using namespace std;
 
+
 /*  Declare Windows procedure  */
-LRESULT CALLBACK WindowProcedure (HWND, UINT, WPARAM, LPARAM);
+LRESULT CALLBACK WindowProcedure(HWND, UINT, WPARAM, LPARAM);
 
 HWND hwnd;               /* This is the handle for our window */
 HWND button[5];
@@ -19,21 +22,26 @@ HWND banda[100];
 HWND tip[100];
 HWND lungime[100];
 HWND latime[100];
-HWND data[100];
+HWND data1[100];
 HWND button_valideaza[100];
 
-int i = 1;
-char textSaved[20];
-
+int i = 0;
+char buffer[10];
+char textEP[20];
+char textTip[20];
+char textLungime[20];
+char textLatime[20];
+char textData[20];
+char textAll[101];
 
 
 /*  Make the class name into a global variable  */
-TCHAR szClassName[ ] = _T("CodeBlocksWindowsApp");
+TCHAR szClassName[] = _T("CodeBlocksWindowsApp");
 
-int WINAPI WinMain (HINSTANCE hThisInstance,
-                    HINSTANCE hPrevInstance,
-                    LPSTR lpszArgument,
-                    int nCmdShow)
+int WINAPI WinMain(HINSTANCE hThisInstance,
+                   HINSTANCE hPrevInstance,
+                   LPSTR lpszArgument,
+                   int nCmdShow)
 {
 
     MSG messages;            /* Here messages to the application are saved */
@@ -44,24 +52,24 @@ int WINAPI WinMain (HINSTANCE hThisInstance,
     wincl.lpszClassName = szClassName;
     wincl.lpfnWndProc = WindowProcedure;      /* This function is called by windows */
     wincl.style = CS_DBLCLKS;                 /* Catch double-clicks */
-    wincl.cbSize = sizeof (WNDCLASSEX);
+    wincl.cbSize = sizeof(WNDCLASSEX);
 
     /* Use default icon and mouse-pointer */
-    wincl.hIcon = LoadIcon (NULL, IDI_APPLICATION);
-    wincl.hIconSm = LoadIcon (NULL, IDI_APPLICATION);
-    wincl.hCursor = LoadCursor (NULL, IDC_ARROW);
+    wincl.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+    wincl.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+    wincl.hCursor = LoadCursor(NULL, IDC_ARROW);
     wincl.lpszMenuName = NULL;                 /* No menu */
     wincl.cbClsExtra = 0;                      /* No extra bytes after the window class */
     wincl.cbWndExtra = 0;                      /* structure or the window instance */
     /* Use Windows's default colour as the background of the window */
-    wincl.hbrBackground = (HBRUSH) COLOR_BACKGROUND;
+    wincl.hbrBackground = (HBRUSH)COLOR_BACKGROUND;
 
     /* Register the window class, and if it fails quit the program */
-    if (!RegisterClassEx (&wincl))
+    if (!RegisterClassEx(&wincl))
         return 0;
 
     /* The class is registered, let's create the program*/
-    hwnd = CreateWindowEx (
+    hwnd = CreateWindowEx(
             0,                   /* Extended possibilites for variation */
             szClassName,         /* Classname */
             _T("Flux Artego"),       /* Title Text */
@@ -77,10 +85,10 @@ int WINAPI WinMain (HINSTANCE hThisInstance,
     );
 
     /* Make the window visible on the screen */
-    ShowWindow (hwnd, nCmdShow);
+    ShowWindow(hwnd, nCmdShow);
 
     /* Run the message loop. It will run until GetMessage() returns 0 */
-    while (GetMessage (&messages, NULL, 0, 0))
+    while (GetMessage(&messages, NULL, 0, 0))
     {
         /* Translate virtual-key messages into character messages */
         TranslateMessage(&messages);
@@ -98,35 +106,35 @@ int WINAPI WinMain (HINSTANCE hThisInstance,
 void scrie() {
 }
 
-LRESULT CALLBACK WindowProcedure (HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
+LRESULT CALLBACK WindowProcedure(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
+    WORD cmd = LOWORD(wParam);
     switch (message)                  /* handle the messages */
     {
         case WM_CREATE:   // fac butoane, label etc
 
-            button[0] =  CreateWindow("BUTTON",
-                                      "Iesire",
-                                      WS_VISIBLE | WS_CHILD | WS_BORDER,
-                                      560, 220, 100, 20,
-                                      hwnd, (HMENU) 1, NULL, NULL);   // (HMENU) 1 reprezinta care case din switch se executa
-            button[1] =  CreateWindow("BUTTON",
-                                      "Adauga",
-                                      WS_VISIBLE | WS_CHILD | WS_BORDER,
-                                      450, 220, 100, 20,
-                                      hwnd, (HMENU) 2, NULL, NULL);
+            button[0] = CreateWindow("BUTTON",
+                                     "Iesire",
+                                     WS_VISIBLE | WS_CHILD | WS_BORDER,
+                                     560, 220, 100, 20,
+                                     hwnd, (HMENU)1, NULL, NULL);   // (HMENU) 1 reprezinta care case din switch se executa
+            button[1] = CreateWindow("BUTTON",
+                                     "Adauga",
+                                     WS_VISIBLE | WS_CHILD | WS_BORDER,
+                                     450, 220, 100, 20,
+                                     hwnd, (HMENU)2, NULL, NULL);
 
 
             break;
 
         case WM_COMMAND:  // fac instructiuni butoane
-
-            switch(LOWORD(wParam) )
+            switch (cmd)
             {
                 case 1:
-//::MessageBeep(MB_ICONERROR);
-//::MessageBox(hwnd, "Ai salvat ?", "atentie", MB_OKCANCEL);
+                    //::MessageBeep(MB_ICONERROR);
+                    //::MessageBox(hwnd, "Ai salvat ?", "atentie", MB_OKCANCEL);
                     cout << "GoodBye!";
-                    PostQuitMessage (0);
+                    PostQuitMessage(0);
                     break;
 
                 case 2:   // Adaug nou record
@@ -152,36 +160,59 @@ LRESULT CALLBACK WindowProcedure (HWND hwnd, UINT message, WPARAM wParam, LPARAM
                                              265, 30 * i, 100, 25,
                                              hwnd, NULL, NULL, NULL);
 
-                    data[i] = CreateWindow("EDIT",
-                                           "Data",
-                                           WS_BORDER | WS_CHILD | WS_VISIBLE,
-                                           370, 30 * i, 100, 25,
-                                           hwnd, NULL, NULL, NULL);
-                    button_valideaza[i] =  CreateWindow("BUTTON",
-                                                        "Scrie",
-                                                        WS_VISIBLE | WS_CHILD | WS_BORDER,
-                                                        475, 30 * i, 80, 20,
-                                                        hwnd, NULL, NULL, NULL);
+                    data1[i] = CreateWindow("EDIT",
+                                            "Data",
+                                            WS_BORDER | WS_CHILD | WS_VISIBLE,
+                                            370, 30 * i, 100, 25,
+                                            hwnd, NULL, NULL, NULL);
+                    button_valideaza[i] = CreateWindow("BUTTON",
+                                                       "Scrie",
+                                                       WS_VISIBLE | WS_CHILD | WS_BORDER,
+                                                       475, 30 * i, 80, 20,
+                                                       hwnd, (HMENU)(i+3), NULL, NULL);
                     i++;
                     break;
 
-                case 3:   // scriu record
-
-
-
-                    int gwtstat = 0;
-                    gwtstat = GetWindowText(banda[i], &textSaved[0], 20);
-
-
+                default:
+                    if (cmd > 2 && cmd < 103)
+                    {
+                        ofstream fout;
+                        fout.open("record.txt", std::ios_base::app);
+                        int gwtstat = 0;
+                        gwtstat = GetWindowText(banda[cmd-3], &textEP[0], 20);
+                        gwtstat = 0;
+                        gwtstat = GetWindowText(tip[cmd-3], &textTip[0], 20);
+                        gwtstat = 0;
+                        gwtstat = GetWindowText(lungime[cmd-3], &textLungime[0], 20);
+                        gwtstat = 0;
+                        gwtstat = GetWindowText(latime[cmd-3], &textLatime[0], 20);
+                        gwtstat = 0;
+                        gwtstat = GetWindowText(data1[cmd-3], &textData[0], 20);
+                        itoa(i, buffer, 10);
+                        strcpy(textAll, buffer);
+                        strcat(textAll," ");
+                        strcat(textAll,textEP);
+                        strcat(textAll, " ");
+                        strcat(textAll,textTip);
+                        strcat(textAll, " ");
+                        strcat(textAll, textLungime);
+                        strcat(textAll, " ");
+                        strcat(textAll, textLatime);
+                        strcat(textAll, " ");
+                        strcat(textAll, textData);
+                        fout << textAll << "\n";
+                        ::MessageBox(hwnd, textAll,"text", MB_OKCANCEL);
+                        fout.close();
+                    }
                     break;
             }
 
             break;
         case WM_DESTROY:
-            PostQuitMessage (0);       /* send a WM_QUIT to the message queue */
+            PostQuitMessage(0);       /* send a WM_QUIT to the message queue */
             break;
         default:                      /* for messages that we don't deal with */
-            return DefWindowProc (hwnd, message, wParam, lParam);
+            return DefWindowProc(hwnd, message, wParam, lParam);
     }
 
     return 0;
